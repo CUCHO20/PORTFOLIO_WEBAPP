96b78d1a2ed50aac157b3a4e60cd584a0067a2ff
CUCHO20 <chu2cho.63@gmail.com>
2025-07-28 04:42:11 -0500

    NEW CLIENTS SECTION ADDED, FIX IT DYNAMIC IMG WIDTH [ISSUE]

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,7 +7,7 @@ import { FaLocationArrow } from 'react-icons/fa'
 
 const Hero = () => {
   return (
-    <div className='pb-20 pt-36'>
+    <section className='pb-20 pt-36'>
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -50,7 +50,7 @@ const Hero = () => {
         </div>
       </div>
 
-    </div>
+    </section>
   )
 }
 
